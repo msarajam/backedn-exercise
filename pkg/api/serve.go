4dd15f7633b6643961f931d349919efdc6b05f1f
Sam Leavens <rbwsam@gmail.com>
2019-06-04 09:49:05 -0700

    Starting fresh

--- pkg/api/serve.go
+++ pkg/api/serve.go
@@ -1,21 +1,12 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"github.com/upbound/backend-exercise/pkg/storage"
 	"gopkg.in/go-playground/validator.v8"
 )
 
-// Serve starts an HTTP API server and listens for incoming requests.
 func Serve(c *storage.Collection, v *validator.Validate) error {
-	r := gin.Default()
-	h := newHandler(c, v)
-
-	r.GET("/apps", h.list)
-	r.GET("/apps/:id", h.fetch)
-	r.POST("/apps", h.create)
-	r.PUT("/apps/:id", h.update)
-	r.DELETE("/apps/:id", h.remove)
-
-	return r.Run() // serve on 0.0.0.0:8080
+	fmt.Println("herrow\ngoodbye")
+	return nil
 }
